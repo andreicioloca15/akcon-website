a4eed4aefc0412e08940dae21cc9c365ab12d4bc
andreicioloca15 <andreicopy15@gmail.com>
2026-01-07 16:14:22 +0200

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,7 +2,6 @@ import Hero from '../components/Hero';
 import BrandPartners from '../components/BrandPartners';
 import About from '../components/About';
 import Services from '../components/Services';
-import WhyChooseUs from '../components/WhyChooseUs';
 import Stats from '../components/Stats';
 import Process from '../components/Process';
 import Testimonials from '../components/Testimonials';
@@ -18,7 +17,6 @@ export default function HomePage() {
       <BrandPartners />
       <About />
       <Services />
-      <WhyChooseUs />
       <Stats />
       <Process />
       <Testimonials />
