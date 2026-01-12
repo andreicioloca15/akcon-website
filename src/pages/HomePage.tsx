7d5cd96ea3c0a81e3f00e3b453dcaec950224be1
andreicioloca15 <andreicopy15@gmail.com>
2026-01-12 19:06:38 +0200

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,7 +5,6 @@ import Services from '../components/Services';
 import Stats from '../components/Stats';
 import Process from '../components/Process';
 import Testimonials from '../components/Testimonials';
-import ProudTransformations from '../components/ProudTransformations';
 import Portfolio from '../components/Portfolio';
 import ServiceArea from '../components/ServiceArea';
 import LocationServices from '../components/LocationServices';
@@ -21,7 +20,6 @@ export default function HomePage() {
       <Stats />
       <Process />
       <Testimonials />
-      <ProudTransformations />
       <Portfolio />
       <ServiceArea />
       <LocationServices />
